fd5184d88a58574c281774311530088068b036a4
Bryan <bryan@fiddle.io>
2020-05-12 12:56:56 +0800

    fix env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import ReactDOM from "react-dom";
+import { get } from "lodash";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import firebase from "./firebase";
+import Spinner from "./Spinner";
 
 import "semantic-ui-css/semantic.min.css";
 
@@ -14,17 +16,22 @@ import {
   withRouter,
 } from "react-router-dom";
 
-import { Provider } from "react-redux";
+import { Provider, connect } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { getStore } from "./config/rootReducer";
+import * as userActions from "./redux/user/actions";
 
 const { store, persistor } = getStore();
 
-let Root = ({ history }) => {
+let Root = (props) => {
+  const { history, setUser, isLoading } = props;
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
+      console.log("hey", user);
+
       if (user) {
-        console.log("user", user);
+        setUser(user);
         history.push("/");
       }
     });
@@ -32,15 +39,27 @@ let Root = ({ history }) => {
   }, []);
 
   return (
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
+    <Fragment>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      )}
+    </Fragment>
   );
 };
 
+const mapStateFromProps = (state) => ({
+  isLoading: get(state, "user.isLoading"),
+});
+
 Root = withRouter(Root);
+Root = connect(mapStateFromProps, userActions)(Root);
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
